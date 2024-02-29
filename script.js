816260116c5af08d0202ca6d8017fc49c3f02290
VishnuThangaraj <vishnuthangaraj.private@gmail.com>
2024-02-29 22:15:27 +0530

    first

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const buttons = [...document.getElementsByClassName("btn")];
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (button.id == "stop") {
+      audio.pause();
+      return;
+    }
+    let audio = new Audio(`./sounds/${button.id}.mp3`);
+    audio.play();
+  });
+});
